00b9eb9828eedb1f29ba7f086e23834ec6cc1ba3
Dale Wylie <dale.wylie@holcim.co.uk>
2025-07-30 10:00:08 +0000

    make the Add criterion button and the select .wav button the same colour

--- src/components/call-review-form.tsx
+++ src/components/call-review-form.tsx
@@ -205,7 +205,7 @@ export default function CallReviewForm() {
                 ))}
               </Accordion>
               <div className="text-center mt-4">
-                <Button variant="outline" onClick={addMatrixItem}>
+                <Button onClick={addMatrixItem} className="bg-[#1d4370] hover:bg-[#1d4370]/90 text-white">
                   <Plus className="mr-2 h-4 w-4" /> Add Criterion
                 </Button>
               </div>
@@ -249,7 +249,7 @@ export default function CallReviewForm() {
                         ref={fileInputRef}
                         className="hidden"
                     />
-                    <Button variant="outline" onClick={() => fileInputRef.current?.click()}>
+                    <Button onClick={() => fileInputRef.current?.click()} className="bg-[#1d4370] hover:bg-[#1d4370]/90 text-white">
                         Select .wav file
                     </Button>
                     {audioFile && (
